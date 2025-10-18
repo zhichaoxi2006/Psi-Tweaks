4133deb952393a3c28f7a9d831c2f04d2113ead6
ZiG60020405 <47026578+zhichaoxi2006@users.noreply.github.com>
2025-10-18 09:35:53 +0800

    version 1.0.1

--- src/main/java/com/zhichaoxi/psi_tweaks/mixin/PlayerDataMixin.java
+++ src/main/java/com/zhichaoxi/psi_tweaks/mixin/PlayerDataMixin.java
@@ -2,10 +2,10 @@ package com.zhichaoxi.psi_tweaks.mixin;
 
 import com.zhichaoxi.psi_tweaks.core.ModAttributes;
 import com.zhichaoxi.psi_tweaks.util.PsiUtil;
-import jdk.jfr.Name;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
